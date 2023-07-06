039905aae863ba76e3559eb9972618aa3312ed44
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-07-05 19:59:14 -0500

    submit problem 4

--- PrepCodeChallenge/Program.cs
+++ PrepCodeChallenge/Program.cs
@@ -6,7 +6,7 @@ class PrepCodeChallenge
     {
         int count = 0;
 
-        for(int i = 0; i < array.Length; i++)
+        for (int i = 0; i < array.Length; i++)
         {
             if (array[i] == n)
             {
@@ -20,9 +20,9 @@ class PrepCodeChallenge
     static bool IsLeapYear(int year)
     {
 
-        if ( year % 4 == 0)
+        if (year % 4 == 0)
         {
-            if( year % 100 == 0 && year % 400 != 0 )
+            if (year % 100 == 0 && year % 400 != 0)
             {
                 return false;
             }
@@ -36,7 +36,7 @@ class PrepCodeChallenge
         int sum = 0;
         int product = 1;
 
-        foreach(int val in array)
+        foreach (int val in array)
         {
             if (val < 0)
             {
@@ -50,6 +50,22 @@ class PrepCodeChallenge
         return sum == product;
     }
 
+    static int[] SumOfRows(int[,] multArray)
+    {
+        int[] rowSumArr = new int[multArray.GetLength(0)];
+
+        for (int i = 0; i < multArray.GetLength(0); i++)
+        {
+            int rowSum = 0;
+            for (int j = 0; j < multArray.GetLength(1); j++)
+            {
+                rowSum += multArray[i, j];
+            }
+            rowSumArr[i] = rowSum;
+        }
+        return rowSumArr;
+
+    }
     static void Main()
     {
         //Problem 1
@@ -79,8 +95,12 @@ class PrepCodeChallenge
         //string yorN = isLeapYear ? "is" : "is not";
         //Console.WriteLine($"{year} {yorN} a leap year");
 
-        int[] seq = { -2, 2, 0 };
-        Console.WriteLine(IsPerfectSequence(seq));
+        int[,] myArray = new int[3, 5] { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 } };
+        int[] sumOfRows = SumOfRows(myArray);
+        foreach (int sum in sumOfRows)
+        {
+            Console.WriteLine(sum);
+        }
         Console.ReadLine();
     }
 
